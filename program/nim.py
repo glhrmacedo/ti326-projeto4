1220d5f99204642040b2fe57f4ff02439639ace0
Guilherme Macedo <guilherme-macedo@outlook.com>
2024-11-07 00:53:03 -0300

    Update nim.py

--- program/nim.py
+++ program/nim.py
@@ -6,12 +6,28 @@ import time
 class Nim():
 
     def __init__(self, initial = [1, 3, 5, 7]):
+
+        '''
+            Initialize game board.
+            Each game board has
+                - 'piles'  : a list of how many elements remain in each pile
+                - 'player' : 0 or 1 to indicate which player's turn
+                - 'winner' : None, 0, or 1 to indicate who the winner is
+        '''
         
         self.piles = initial.copy()
         self.player = 0
         self.winner = None
 
     def avaliable_actions(self, piles):
+        
+        '''
+            self.avaliable_actions(piles) takes a 'piles' list as input
+            and returns all of the available actions '(i, j)' int that state.
+
+            Action '(i, j)' represents the action of removing 'j' items
+            from pile 'i'.
+        '''
 
         actions = set()
 
@@ -23,13 +39,27 @@ class Nim():
 
     def other_player(self, player):
 
+        '''
+            self.other_player(plauyer) returns the player that is not
+            'player'. Assumes 'player' is either 0 or 1.
+        '''
+
         return 0 if player == 1 else 1
 
     def switch_player(self):
 
+        '''
+            Switch the current player to the other player.
+        '''
+
         self.player = self.other_player(self.player)
 
     def move(self, action):
+        
+        '''
+            Make the move 'action' for the current player.
+            'action' must be a tuple '(i, j)'.
+        '''
 
         pile, count = action
 
@@ -55,26 +85,66 @@ class Nim():
 class SARSA():
 
     def __init__(self, alpha = 0.5, epsilon = 0.1):
-        
-        self.q = {}
+
+        '''
+            Initialize AI with an empty SARSA dictionary,
+            an alpha rate and an epsilon rate.
+
+            The SARSA dictionary maps '(state, action)
+            pairs to a Q-value.
+                - 'state' is a tuple of remaining piles, e.g. [1, 1, 4, 4]
+                - 'action' is a tuple '(i, j)' for an action
+        '''
+
+        self.q = dict()
         self.alpha = alpha
         self.epsilon = epsilon
 
     def update(self, old_state, action, new_state, reward):
 
-        pass
+        '''
+            Update SARSA model, given and old state, an action taken
+            in that state, a new resulting state, and the reward received
+            from taking that action.
+        '''
+
+        old_q = self.get_value(old_state, action)
+        best_future = self.best_future_reward(new_state)
+        self.update_value(old_state, action, old_q, reward, best_future)
 
     def get_value(self, state, action):
 
+        '''
+            Return the Q-value for the state 'state' and the action 'action'.
+            If no Q-value exists yet in 'self.q', return 0.
+        '''        
+
         raise NotImplementedError
 
-    def update_value(self, state, action):
+    def update_value(self, old_state, action, old_q, reward, future_rewards):
 
-        raise NotImplementedError
+        '''
+            Update the Q-value for the state 'state' and the action 'action'
+            given the previous Q-value 'old_q', a current reward 'reward',
+            and an estimate of future rewards 'future_rewards'.
+        '''
 
     def choose_action(self, state, epsilon = True):
 
-        raise NotImplementedError
+        '''
+            Given a state 'state', return a action '(i, j)' to take.
+            
+            If 'epsilon' is 'False', then return the best action
+            avaiable in the state (the one with the highest Q-value, 
+            using 0 for pairs that have no Q-values).
+
+            If 'epsilon' is 'True', then with probability 'self.epsilon'
+            chose a random available action, otherwise chose the best
+            action available.
+
+            If multiple actions have the same Q-value, any of those
+            options is an acceptable return value.
+        '''
 
 
 class QLearning():
