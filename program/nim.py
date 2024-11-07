42953dc072ccc8953264f8fcd4f52a8b7bb94ecb
Guilherme Macedo <guilherme-macedo@outlook.com>
2024-11-07 00:45:26 -0300

    Update nim.py

--- program/nim.py
+++ program/nim.py
@@ -81,26 +81,81 @@ class QLearning():
 
     def __init__(self, alpha = 0.5, epsilon = 0.1):
 
+        '''
+            Initialize AI with an empty Q-learning dictionary,
+            an alpha rate and an epsilon rate.
+
+            The Q-learning dictionary maps '(state, action)
+            pairs to a Q-value.
+                - 'state' is a tuple of remaining piles, e.g. [1, 1, 4, 4]
+                - 'action' is a tuple '(i, j)' for an action
+        '''
+
+        self.q = dict()
         self.alpha = alpha
         self.epsilon = epsilon
 
     def update(self, old_state, action, new_state, reward):
 
-        pass
+        '''
+            Update Q-learning model, given and old state, an action taken
+            in that state, a new resulting state, and the reward received
+            from taking that action.
+        '''
+
+        old_q = self.get_value(old_state, action)
+        best_future = self.best_future_reward(new_state)
+        self.update_value(old_state, action, old_q, reward, best_future)
 
     def get_value(self, state, action):
 
+        '''
+            Return the Q-value for the state 'state' and the action 'action'.
+            If no Q-value exists yet in 'self.q', return 0.
+        '''
+
         raise NotImplementedError
 
-    def update_value(self, state, action):
+    def update_value(self, old_state, action, old_q, reward, future_rewards):
+
+        '''
+            Update the Q-value for the state 'state' and the action 'action'
+            given the previous Q-value 'old_q', a current reward 'reward',
+            and an estimate of future rewards 'future_rewards'.
+        '''
 
         raise NotImplementedError
 
     def best_future_reward(self, state):
 
+        '''
+            Given a state 'state', consider all possible '(state, action)'
+            pairs available in that state and return the maximum of all
+            of their Q-values.
+
+            Use 0 as the Q-value if a '(state, action)' pair has no
+            Q-value in 'self.q'. If there are no available actions
+            in 'state', return 0.
+        '''
+
         raise NotImplementedError
 
     def choose_action(self, state, epsilon = True):
+
+        '''
+            Given a state 'state', return a action '(i, j)' to take.
+            
+            If 'epsilon' is 'False', then return the best action
+            avaiable in the state (the one with the highest Q-value, 
+            using 0 for pairs that have no Q-values).
+
+            If 'epsilon' is 'True', then with probability 'self.epsilon'
+            chose a random available action, otherwise chose the best
+            action available.
+
+            If multiple actions have the same Q-value, any of those
+            options is an acceptable return value.
+        '''
 
         raise NotImplementedError
 
